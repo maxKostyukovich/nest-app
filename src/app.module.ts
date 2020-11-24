2a16aee751144238070c3c4686d7dfc1a1684018
Max Kostyukovych <max.aq2@gmail.com>
2020-11-24 22:29:42 +0200

    Setup simple crud api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import * as process from "process";
+
+const username = process.env.POSTGRES_USER || 'User';
+const password = process.env.POSTGRES_PASSWORD || 'kmk123';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username,
+      password,
+      database: 'nest-db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
